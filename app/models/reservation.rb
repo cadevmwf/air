aa11e765a4facbba9a1ae8e3773d5a9b30968470
Jeff Cohen <jeff@codeacademy.org>
2012-08-13 09:10:54 -0500

    Use a model callback to update the miles_earned for the user

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -8,4 +8,11 @@ class Reservation < ActiveRecord::Base
   validates :departs_on, :presence => true
   validates :card_number, :format => { :with => /^4\d{15}/ }
   
+  after_create :increase_frequent_flyer_miles
+  
+  def increase_frequent_flyer_miles
+    self.user.miles_earned += self.flight.distance
+    self.user.save
+  end
+  
 end
