7f4d91ffe121993fd1d0fb463d5be56ebaa76f62
Jeff Cohen <jeff@codeacademy.org>
2012-08-15 09:33:28 -0500

    Added pagination to flights index. Updated seeds file.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,17 +1,28 @@
 class FlightsController < ApplicationController
 
   def index
+    
     if params[:keyword].present?
+      
       @flights = Flight.where("LOWER(departure_airport_code) = ? OR LOWER(arrival_airport_code) = ?", params[:keyword].downcase, params[:keyword].downcase)
+      
+      @flights = @flights.page(params[:page]).per(3)
+      
+    else
+      
+      @flights = Flight.page(params[:page]).per(3)
+      
     end
-
-    # Flight.offset(2).limit(2)
-    @page_number = params[:page].to_i
-    @page_number = 1 if @page_number < 1
     
-    @flights = Flight.limit(4) if @flights.nil?
     
-    @flights = @flights.offset((@page_number - 1) * 4).limit(4)
+    # 
+    # # Flight.offset(2).limit(2)
+    # @page_number = params[:page].to_i
+    # @page_number = 1 if @page_number < 1
+    # 
+    # @flights = Flight.limit(4) if @flights.nil?
+    # 
+    # @flights = @flights.offset((@page_number - 1) * 4).limit(4)
     
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,12 @@ if Rails.env.development?
   Time.zone = "America/Chicago"
   
   Flight.destroy_all
+  User.destroy_all
+  Reservation.destroy_all
   
   codes = ['ORD', 'JFK', 'BOS', 'LAX']
   
+  puts 'Creating flights.'
   100.times do 
     departure_code, arrival_code = codes.sample(2)
     departure_time = rand(8..20)
@@ -18,7 +21,36 @@ if Rails.env.development?
                   :departure_airport_code => departure_code, 
                   :arrival_airport_code => arrival_code, 
                   :departs_at => (Date.today + departure_time.hours),
-                  :distance => MileageCalculator.new(departure_code, arrival_code).miles
+                  :distance => MileageCalculator.new(departure_code, arrival_code).miles       
   end
+  
+  puts 'Creating users.'
+  50.times do |i|
+    User.create :email => "user#{i}@gmail.com",
+                :name => "First#{i} Last#{i}",
+                :password => 'test',
+                :password_confirmation => 'test'
+  end
+  
+  puts 'Creating reservations.'
+  # :card_number, :departs_on, :flight_id, :user_id
+  
+  flights = Flight.all
+  User.all.each do |user|
+    flights.sample(rand(5)).each do |flight|
+      Reservation.create :flight_id => flight.id,
+                         :user_id => user.id,
+                         :card_number => (4444000000000000 + rand(1000000000000)).to_s,
+                         :departs_on => Date.today + rand(180)
+    end
+  end
+  
+  
+  
+  
+  
+  
+  
+  
   
 end
