d409c56e29eae176642e822e63d874ccc9ed610f
Jeff Cohen <jeff@codeacademy.org>
2012-08-15 10:40:52 -0500

    Updated seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,27 @@ if Rails.env.development?
   Flight.destroy_all
   User.destroy_all
   Reservation.destroy_all
+  Airport.destroy_all
   
-  codes = ['ORD', 'JFK', 'BOS', 'LAX']
+  puts 'Creating airports.'
+  airports = [
+    {:code => 'ORD', :city => 'Chicago'},
+    {:code => 'JFK', :city => 'New York'},
+    {:code => 'BOS', :city => 'Boston'},
+    {:code => 'LAX', :city => 'Los Angeles'},
+  ]
+  
+  airports.each do |airport_hash|
+    Airport.create airport_hash
+  end
   
   puts 'Creating flights.'
-  100.times do 
-    departure_code, arrival_code = codes.sample(2)
+  all_airports = Airport.all
+  100.times do
+    random_airports = all_airports.sample(2)
+    departure_airport = random_airports.first
+    arrival_airport = random_airports.last
+        
     departure_time = rand(8..20)
     flight_number = rand(100..999)
 
@@ -18,10 +33,10 @@ if Rails.env.development?
     # Maybe something like the code below:
     
     Flight.create :number => flight_number,
-                  :departure_airport_code => departure_code, 
-                  :arrival_airport_code => arrival_code, 
+                  :departure_airport => departure_airport, 
+                  :arrival_airport => arrival_airport, 
                   :departs_at => (Date.today + departure_time.hours),
-                  :distance => MileageCalculator.new(departure_code, arrival_code).miles       
+                  :distance => MileageCalculator.new(departure_airport.code, arrival_airport.code).miles       
   end
   
   puts 'Creating users.'
